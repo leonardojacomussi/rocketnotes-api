e5e3832d5180430fcfa3b617b2cfdc6fd7688fdc
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-25 17:32:54 -0300

    FIX: adjust knex environment

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,6 @@
 const config = require("../../../knexfile");
 const knex = require("knex");
 
-const connection = knex(config.development);
+const connection = knex(process.env.NODE_ENV === "production" ? config.production : config.development);
 
 module.exports = connection;
